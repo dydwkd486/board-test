e565708248c7483bcfb69d8756b4214bdb4c309e
박신희 <psh7130@naver.com>
2022-11-13 18:12:21 +0900

    [update] add a comment

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import BoardView from '../views/BoardView'
-import AboutView from '../views/AboutView'
-import ListPost from '../components/ListPost'
-import RegistPost from '../components/RegistPost'
-import ModifyPost from '../components/ModifyPost'
+import HomeView from '@/views/HomeView.vue'
+import BoardView from '@/views/BoardView'
+import AboutView from '@/views/AboutView'
+import ListPost from '@/components/ListPost'
+import RegistPost from '@/components/RegistPost'
+import ModifyPost from '@/components/ModifyPost'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
+  { // 메인 페이지
     path: '/',
     name: 'home',
     component: HomeView
@@ -20,25 +20,25 @@ const routes = [
     name: 'board',
     component: BoardView,
     children: [
-      {
+      { // 목록 조회
         path: '/',
         name: 'list',
         component: ListPost,
       },
-        {
-          path : '/regist',
-          name : 'regist',
-          component: RegistPost,
-        },
-        {
-          path : '/modify/:no',
-          name : 'modify',
-          component : ModifyPost,
-          props : true
-        },
+      { // 글 등록
+        path : '/regist',
+        name : 'regist',
+        component: RegistPost,
+      },
+      { // 글 수정
+        path : '/modify/:no',
+        name : 'modify',
+        component : ModifyPost,
+        props : true
+      },
     ],
   },
-  {
+  { // 팀원 소개
     path: '/about',
     name: 'about',
     component: AboutView
